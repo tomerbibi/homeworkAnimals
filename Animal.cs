6412f0c46d050cd09fe18468eaad2edae7dacb73
tomerbibi <66681821+tomerbibi@users.noreply.github.com>
2020-07-07 18:33:23 +0300

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeworkAnimals
+{
+   abstract class Animal
+    {
+        public string _name;
+
+        public Animal(string name)
+        {
+            _name = name;
+        }
+        public abstract void MakeSound();
+
+        public override string ToString()
+        {
+            return $"name: {_name}";
+        }
+    }
+}
